21aab7b899b86c3f86289dd52b3f409cb06618dd
mengniu121 <jian.gao@enapir.com>
2025-04-09 18:00:52 +0900

    环境变量更新

--- config.py
+++ config.py
@@ -8,16 +8,16 @@ SOURCE_DB_CONFIG = {
     'driver': 'SQL Server',
     'server': os.getenv('SOURCE_DB_SERVER'),
     'database': os.getenv('SOURCE_DB_NAME'),
-    'uid': os.getenv('DB_USER'),  # SQL Server認証ユーザー名
-    'pwd': os.getenv('DB_PASSWORD')  # SQL Server認証パスワード
+    'uid': os.getenv('SOURCE_DB_USER'),  # SQL Server認証ユーザー名
+    'pwd': os.getenv('SOURCE_DB_PASSWORD')  # SQL Server認証パスワード
 }
 
 TARGET_DB_CONFIG = {
     'driver': 'SQL Server',
     'server': os.getenv('TARGET_DB_SERVER'),
     'database': os.getenv('TARGET_DB_NAME'),
-    'uid': os.getenv('DB_USER'),  # SQL Server認証ユーザー名
-    'pwd': os.getenv('DB_PASSWORD')  # SQL Server認証パスワード
+    'uid': os.getenv('TARGET_DB_USER'),  # SQL Server認証ユーザー名
+    'pwd': os.getenv('TARGET_DB_PASSWORD')  # SQL Server認証パスワード
 }
 
 # データ型マッピング
